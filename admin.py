a7cf7c8f88a61d603409aa13cd05b4991de956b2
Thomas Uher <thomas.uher@sisol-systems.com>
2020-09-22 12:23:09 +0200

    MetaApp in admin

--- admin.py
+++ admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django_tenants.admin import TenantAdminMixin
 
 from .models import MetaApp
 
-@admin.register(MetaApp)
-class MetaAppAdmin(TenantAdminMixin, admin.ModelAdmin):
+class MetaAppAdmin(admin.ModelAdmin):
     pass
+
+admin.site.register(MetaApp, MetaAppAdmin)
