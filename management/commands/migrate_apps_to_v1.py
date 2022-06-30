28607c2ccef0a9e687d7423446afa11cb6b238f3
Thomas Uher <thomas.uher@sisol-systems.com>
2022-06-30 15:08:09 +0200

    frontend migration management command

--- management/commands/migrate_apps_to_v1.py
+++ management/commands/migrate_apps_to_v1.py
@@ -8,7 +8,7 @@ from django.contrib.contenttypes.models import ContentType
 
 from django.conf import settings
 
-import os
+import os, sys
 
 '''
     Migrate all apps to the new frontend layout
@@ -20,16 +20,25 @@ class Command(BaseCommand):
 
 
     def _create_frontend(self, meta_app):
-        
-        feature_name = str(Frontend._meta.verbose_name)
-        feature = Frontend.objects.create(feature_name, meta_app.primary_language)
 
-        link = MetaAppGenericContent(
-            meta_app = meta_app,
-            content_type = ContentType.objects.get_for_model(feature),
-            object_id = feature.id,
-        )
-        link.save()
+        frontend_content_type = ContentType.objects.get_for_model(feature)
+
+        frontend_exists = MetaAppGenericContent.objects.filter(meta_app = meta_app,
+            content_type = frontend_content_type).exists()
+
+        if not frontend_exists:
+        
+            feature_name = str(Frontend._meta.verbose_name)
+            feature = Frontend.objects.create(feature_name, meta_app.primary_language)
+
+
+            link = MetaAppGenericContent(
+                meta_app = meta_app,
+                content_type = frontend_content_type,
+                object_id = feature.id,
+            )
+            link.save()
+
 
     def handle(self, *args, **options):
 
@@ -49,20 +58,29 @@ class Command(BaseCommand):
             new_folder_name = '{0}_old'.format(str(meta_app.uuid))
             new_app_root = os.path.join(settings.APP_KIT_ROOT, new_folder_name)
 
-            self.stdout.write('Moving {0} -> {1}'.format(app_root, new_app_root))
+            if os.path.isdir(new_app_root):
+                self.stdout.write('{1} already exists'.format(new_app_root))
+            else:
+                self.stdout.write('Moving {0} -> {1}'.format(app_root, new_app_root))
 
-            os.rename(app_root, new_app_root)
+                os.rename(app_root, new_app_root)
 
-            # Step 3: recreate the app version on disk
-            self.stdout.write('Creating app version {0} for {1}.'.format(meta_app.current_version, meta_app.name))
-            app_builder = meta_app.get_app_builder()
-            app_builder.create_app_version()
-            self.stdout.write('Finished creating app version {0} build for {1}.'.format(meta_app.current_version,
-                                                                                    meta_app.name))
+            if os.path.isdir(app_root):
+                self.stdout.write('old app directory {1} STILL  exists. Aborting.'.format(app_root))
+                sys.exit(1)
 
-            # Step 4: recreate preview
-            self.stdout.write('Creating preview version for {0}.'.format(meta_app.name))
-            preview_builder = meta_app.get_preview_builder()
-            preview_builder.build()
+            else:
 
-            self.stdout.write('Finished creating preview build for {0}.'.format(meta_app.name))
+                # Step 3: recreate the app version on disk
+                self.stdout.write('Creating app version {0} for {1}.'.format(meta_app.current_version, meta_app.name))
+                app_builder = meta_app.get_app_builder()
+                app_builder.create_app_version()
+                self.stdout.write('Finished creating app version {0} build for {1}.'.format(meta_app.current_version,
+                                                                                        meta_app.name))
+
+                # Step 4: recreate preview
+                self.stdout.write('Creating preview version for {0}.'.format(meta_app.name))
+                preview_builder = meta_app.get_preview_builder()
+                preview_builder.build()
+
+                self.stdout.write('Finished creating preview build for {0}.'.format(meta_app.name))
