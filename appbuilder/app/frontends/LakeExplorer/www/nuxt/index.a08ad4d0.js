daef7f961da42a1c5425fccb69325310da491b8b
Thomas Uher <thomas.uher@sisol-systems.com>
2022-09-29 12:50:16 +0200

    add LakeExplorer frontend

---
+++ appbuilder/app/frontends/LakeExplorer/www/nuxt/index.a08ad4d0.js
@@ -0,0 +1,1 @@
+import{b as e,o as n,e as o}from"./entry.e4685ebd.js";const t={name:"Index"};function s(a,c,r,p,l,_){return n(),o("span",null,"Null")}const f=e(t,[["render",s]]);export{f as default};
