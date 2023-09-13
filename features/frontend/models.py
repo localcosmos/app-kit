2a85fc3b39525a8b62b664ce6849c2f99502ad79
Thomas Uher <thomas.uher@sisol-systems.com>
2023-09-13 22:15:58 +0200

    frontend name

--- features/frontend/models.py
+++ features/frontend/models.py
@@ -37,8 +37,7 @@ class Frontend(SingleFeatureMixin, ContentImageMixin, GenericContent):
 
         locale['_meta'] = {}
 
-        frontend_texts = FrontendText.objects.filter(frontend=self, frontend_name=self.frontend_name,
-            frontend_name=self.frontend_name)
+        frontend_texts = FrontendText.objects.filter(frontend=self, frontend_name=self.frontend_name)
 
         for text in frontend_texts:
             locale_key = 'frontend-text-{0}'.format(text.identifier) 
